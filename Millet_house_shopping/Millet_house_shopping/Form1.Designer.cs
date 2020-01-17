4e896efbed2f89d15ceb98e37900195418345ee5
dzj1995 <1543574315@qq.com>
2020-01-17 16:34:53 +0800

    Add files via upload

---
+++ Millet_house_shopping/Millet_house_shopping/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Millet_house_shopping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.START_BUTTON = new System.Windows.Forms.Button();
+            this.product_price = new System.Windows.Forms.TextBox();
+            this.input_message = new System.Windows.Forms.TextBox();
+            this.product_kind = new System.Windows.Forms.TextBox();
+            this.total_money = new System.Windows.Forms.TextBox();
+            this.result_textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // START_BUTTON
+            // 
+            this.START_BUTTON.Location = new System.Drawing.Point(12, 12);
+            this.START_BUTTON.Name = "START_BUTTON";
+            this.START_BUTTON.Size = new System.Drawing.Size(106, 23);
+            this.START_BUTTON.TabIndex = 0;
+            this.START_BUTTON.Text = "START";
+            this.START_BUTTON.UseVisualStyleBackColor = true;
+            this.START_BUTTON.Click += new System.EventHandler(this.START_BUTTON_Click);
+            // 
+            // product_price
+            // 
+            this.product_price.Location = new System.Drawing.Point(12, 75);
+            this.product_price.Name = "product_price";
+            this.product_price.Size = new System.Drawing.Size(106, 25);
+            this.product_price.TabIndex = 2;
+            this.product_price.Text = "每种产品价格";
+            // 
+            // input_message
+            // 
+            this.input_message.Location = new System.Drawing.Point(124, 41);
+            this.input_message.Multiline = true;
+            this.input_message.Name = "input_message";
+            this.input_message.Size = new System.Drawing.Size(146, 95);
+            this.input_message.TabIndex = 0;
+            // 
+            // product_kind
+            // 
+            this.product_kind.Location = new System.Drawing.Point(12, 41);
+            this.product_kind.Name = "product_kind";
+            this.product_kind.Size = new System.Drawing.Size(106, 25);
+            this.product_kind.TabIndex = 4;
+            this.product_kind.Text = "产品种类数";
+            this.product_kind.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // total_money
+            // 
+            this.total_money.Location = new System.Drawing.Point(11, 106);
+            this.total_money.Name = "total_money";
+            this.total_money.Size = new System.Drawing.Size(107, 25);
+            this.total_money.TabIndex = 5;
+            this.total_money.Text = "预算金额";
+            this.total_money.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // result_textBox1
+            // 
+            this.result_textBox1.Location = new System.Drawing.Point(281, 41);
+            this.result_textBox1.Multiline = true;
+            this.result_textBox1.Name = "result_textBox1";
+            this.result_textBox1.Size = new System.Drawing.Size(164, 95);
+            this.result_textBox1.TabIndex = 6;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(281, 12);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(164, 23);
+            this.textBox6.TabIndex = 7;
+            this.textBox6.Text = "计算结果";
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 23);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "输入数据";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 146);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.result_textBox1);
+            this.Controls.Add(this.total_money);
+            this.Controls.Add(this.product_kind);
+            this.Controls.Add(this.input_message);
+            this.Controls.Add(this.product_price);
+            this.Controls.Add(this.START_BUTTON);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.init_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button START_BUTTON;
+        private System.Windows.Forms.TextBox product_price;
+        private System.Windows.Forms.TextBox input_message;
+        private System.Windows.Forms.TextBox product_kind;
+        private System.Windows.Forms.TextBox total_money;
+        private System.Windows.Forms.TextBox result_textBox1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Millet_house_shopping/Millet_house_shopping/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Millet_house_shopping
+{
+    public partial class Form1 : Form
+    {
+        private Thread trd;
+        static AutoResetEvent myResetEvent = new AutoResetEvent(false);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void ThreadTask()
+        {
+            input_message.Text += 2 + "\r\n";
+            input_message.Text += 500 + "\r\n";
+            input_message.Text += 1 + "\r\n";
+            input_message.Text += 1000 + "\r\n";
+            input_message.Select(input_message.TextLength - 1, 0);
+            while (true)
+            {               
+                myResetEvent.WaitOne();//点击按钮后，程序开始由此执行  
+                result_textBox1.Clear();
+                int _prices_size = 0;
+                _prices_size = Convert.ToInt32(input_message.Lines[0]);
+                string[] arr = new string[_prices_size+2];
+
+                for (int i = 0; i < arr.Length; i++)//读取各种商品的单价为字符
+                {
+                    arr[i] = input_message.Lines[i];
+                    //result_textBox1.Text += arr[i] + "\r\n";
+                }
+
+                int _budget = Convert.ToInt32(arr[_prices_size + 1]);//获取商品预算
+                int[] dp = new int[_budget + 1];
+                for (int i = 0; i < dp.Length; i++)
+                {
+                    dp[i] = int.MaxValue - 1;
+                }
+
+                int[] _prices = new int[_prices_size];
+                for (int _prices_i = 0; _prices_i < _prices_size; _prices_i++)//将商品单价字符转化为整形
+                {                   
+                    _prices[_prices_i] = Convert.ToInt32(arr[_prices_i+1]);
+                    if (_prices[_prices_i]<=_budget)//由于dp[i]数组大小有限制，因此必须限制i小于预算值
+                    {
+                        dp[_prices[_prices_i]] = 1;//然后让商品价格等于预算值时，可以买到商品数量的值设置为1
+                    }
+                }
+
+                for (int i = 1; i <= _budget; i++)
+                {
+                    if (dp[i] == int.MaxValue - 1)
+                    {
+                        for (int j = 0; j < _prices.Length; j++)
+                        {
+                            if (_prices[j] < i)
+                            {
+                                dp[i] = Math.Min(dp[i], dp[i - _prices[j]] + 1);
+                            }
+                        }
+                    }
+                }
+                dp[_budget]=dp[_budget] == int.MaxValue - 1 ? -1 : dp[_budget];
+                result_textBox1.Text += "能买产品数为" + dp[_budget] + "\r\n";
+            }
+        }
+
+        private void init_Load(object sender, EventArgs e)
+        {
+            trd = new Thread(new ThreadStart(this.ThreadTask));
+            trd.IsBackground = true;
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+            trd.Priority = ThreadPriority.Highest;
+            trd.Start();
+        }
+
+        private void START_BUTTON_Click(object sender, EventArgs e)
+        {
+            myResetEvent.Set();//点击按钮后释放线程的等待   
+        }
+    }
+}
+
+
